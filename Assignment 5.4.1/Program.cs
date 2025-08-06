ec12698ea2f5dd57960a57d56567000f26ceb051
MarkMolabola <mark.molabola@outlook.com>
2025-08-06 13:56:51 -0700

    Add project files.

---
+++ Assignment 5.4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_5._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1234;
+            Console.WriteLine(number);
+            DisplayNumbers(number);
+
+        }
+        static void DisplayNumbers(int number)
+        {
+            if (number > 0)
+            {
+                int result = number % 10;
+                DisplayNumbers(number/=10);
+                
+                Console.WriteLine(result);
+                
+            }
+            
+
+        }
+    }
+}
